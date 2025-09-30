918ca840612d39c7643afa9a7b8c5f4ecd16354a
jamesqquick <james.q.quick@gmail.com>
2025-09-30 09:23:43 -0500

    separate client into its own file

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,32 +1,12 @@
 import { openai } from "@ai-sdk/openai";
 import {
-  convertToModelMessages, experimental_createMCPClient, experimental_MCPClient, streamText, UIMessage
+  convertToModelMessages,
+  experimental_createMCPClient,
+  experimental_MCPClient,
+  streamText,
+  UIMessage,
 } from "ai";
-import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
-
-let mcpClient: experimental_MCPClient | null = null;
-
-async function getClient() {
-  if (!process.env.MCP_SERVER_URL) {
-    console.error("MCP_SERVER_URL is not set");
-
-    throw new Response("MCP_SERVER_URL is not set", { status: 500 });
-  }
-
-  if (mcpClient) {
-    return mcpClient;
-  }
-
-  const transport = new StreamableHTTPClientTransport(
-    new URL(process.env.MCP_SERVER_URL),
-  );
-
-  mcpClient = await experimental_createMCPClient({
-    transport,
-  });
-
-  return mcpClient;
-}
+import { getClient } from "@/lib/mcpClient";
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
