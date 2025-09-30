f811b57fce0c1195ccf8c612391583ecf6c49103
jamesqquick <james.q.quick@gmail.com>
2025-09-30 09:00:37 -0500

    update starter for api route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,4 @@
 export async function POST(req: Request) {
-  try {
-    return new Response("Hello, world!", { status: 200 });
-  } catch (error) {
-    console.error("Error in POST /api/chat:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
+  //TODO: implement chat route
+  return new Response("Internal Server Error", { status: 500 });
 }
